de356fc365fed9b546ad8e12e18c9c3794751135
Paolo Sanchez <psan@netcompany.com>
2024-05-10 17:42:46 +0200

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
+const LIVE_URL = "https://psanchez-it.github.io" 
+
+const BASE = "/allmylinks-main"
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://allmylinks-ten.vercel.app/",
+  site: LIVE_URL,
+  base: BASE,
   prefetch: true
 });
